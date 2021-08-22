953d64e08002b0f1f90975fb1c04f8f4c2e87e97
mich <m.ferrante.mi@gmail.com>
2021-08-22 19:59:49 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+find-files () {
+	find . -type f -not -name 'install.sh' -not -name '.git*' -not -path '*/.git/*' -not -path '*/nvim/*'
+}
+
+cd ${0//install.sh/}
+
+for file in $(find-files)
+do
+	orig=${file/./"$(pwd)"}
+	dest=${file/./$HOME}
+
+	if [[ -f "$dest" && "$(stat -c %h -- "$dest")" -lt 2 ]]
+	then
+		echo "Moving old ${file/.\//} to ${file/.\//}.bak"
+		mv -f "$dest" "$dest.bak"
+	fi
+
+	if [[ ! -d $(dirname "$dest") ]]
+	then
+		mkdir -p $(dirname "$dest")
+	fi
+
+	ln $orig $dest
+done
+
+if [[ ! -d "$HOME/.config/nvim" ]]
+then
+	ln -s "$(pwd)/.config/nvim" "$HOME/.config/nvim"
+fi
+
